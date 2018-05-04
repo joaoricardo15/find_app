5b63ee7b14fd996ed414e3cfaefcb0aad0dcdd26
joaoricardo15 <joaoricardo15@hotmail.com>
2018-05-04 12:27:21 -0300

    working2

--- platforms/android/app/build/intermediates/assets/debug/www/js/index.js
+++ platforms/android/app/build/intermediates/assets/debug/www/js/index.js
@@ -1,60 +1,37 @@
 var server = true;
-var myId;
-var myLastId;
 var url = "https://findserver.azurewebsites.net/positions";
-var localPositions = [];
-var updateInterval = 1000;
-var uploadMyPositionTimer;
-var updatePositionsTimer;
-var uploadInterval = 1000;
 var minimumPrecision = 0.0001;
+var updateInterval = 1000;
+var updatePositionsTimer;
+var localPositions = [];
+var positionWatch;
 var myPosition;
-var myLastPosition;
+var myId;
 var map;
 
-function initMap() {
-    navigator.geolocation.getCurrentPosition((position) => {
-        
-        myPosition = {lat: position.coords.latitude, lng: position.coords.longitude};
+function onMapReady() {
+    
+    myId = window.localStorage.getItem('myId');
 
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: myPosition,
-            zoom: 15
-        });
-    });
+    if(myId)
+        initMap();
+    else 
+        initLogin();    
 }
 
-//function onSuccess(position) {}
-
-//function onError(error) {}
-
-//var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
-
-function uploadMyPosition()
+function uploadMyPosition(position)
 {
-    navigator.geolocation.getCurrentPosition((position) => {
+    myPosition = {lat: position.coords.latitude, lng: position.coords.longitude};
+    
+    var data = {id: myId, lat: myPosition.lat, lng: myPosition.lng};
 
-        myPosition = {lat: position.coords.latitude, lng: position.coords.longitude};
-
-        if(!myLastPosition || myId != myLastId || Math.abs(myPosition.lat - myLastPosition.lat) > minimumPrecision || Math.abs(myPosition.lng - myLastPosition.lng) > minimumPrecision)
-        {
-            var data = {id: myId, lat: myPosition.lat, lng: myPosition.lng};
-
-            $.ajax({
-                url: url,
-                type: "POST",
-                data: JSON.stringify(data),
-                dataType: "json",
-                contentType: "application/json; charset=utf-8",
-                success: function(data, status){}
-            });
-
-            myLastPosition = myPosition;
-
-            if(myId != myLastId)
-                myLastId = myId;
-        }
-        
+    $.ajax({
+        url: url,
+        type: "POST",
+        data: JSON.stringify(data),
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success: function(data, status){}
     });
 }
 
@@ -65,107 +42,166 @@ function updatePositions()
         type: "GET",
         dataType: "json",
         contentType: "application/json; charset=utf-8",
-        success: function(data, status){
-            updateMarkers(data);
-        }
+        success: function(data, status){ updateMarkers(data) }
     });
 }
 
 function updateMarkers(positions)
 {
-    for(var position of positions)
+    if(positions.length > 0)
     {
-        if(position.id)    
+        for(var position of positions)
         {
-            var index = localPositions.findIndex(localPosition => localPosition.id === position.id);
-            
-            if(index > -1)
+            if(position.id)    
             {
-                var lat = localPositions[index].marker.getPosition().lat();
-                var lng = localPositions[index].marker.getPosition().lng();
-            
-                if(Math.abs(lat - position.lat) > minimumPrecision || Math.abs(lng - position.lng) > minimumPrecision)
-                    localPositions[index].marker.setPosition({lat: position.lat, lng: position.lng});
-            }
-            else
-            {
-                var alphabet = [ 'a', 'b', 'c', 'd', 'e',
-                   'f', 'g', 'h', 'i', 'j',
-                   'k', 'l', 'm', 'n', 'o',
-                   'p', 'q', 'r', 's', 't',
-                   'u', 'v', 'w', 'x', 'y',
-                   'z' ];
+                var index = localPositions.findIndex(localPosition => localPosition.id === position.id);
+                
+                if(index > -1)
+                {
+                    var lat = localPositions[index].marker.getPosition().lat();
+                    var lng = localPositions[index].marker.getPosition().lng();
+                    
 
-                var index = alphabet.findIndex(letter => letter === position.id[0].toLowerCase());
+                    if(Math.abs(lat - position.lat) > minimumPrecision || Math.abs(lng - position.lng) > minimumPrecision)
+                        localPositions[index].marker.setPosition({lat: position.lat, lng: position.lng});
+                }
+                else
+                {
+                    var alphabet = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-                if(index < 1)
-                    index = 1;
+                    var index = alphabet.findIndex(letter => letter === position.id[0].toLowerCase());
 
-                var zIndex = index * position.id.length;
+                    if(index < 1)
+                        index = 1;
 
-                var marker = new google.maps.Marker({
-                    map: map,
-                    title: 'Nome do herói: '+position.id,
-                    position: {
-                        lat: position.lat,
-                        lng: position.lng
-                    },
-                    label: {
-                        //color: '',
-                        //fontFamily: '',
-                        fontSize: '18px',
-                        fontWeight: '2px',
-                        text: position.id.substring(0, 3)
-                    },
-                    //label: '<div style="font-size:20px;">'+position.id+'</div>',
-                    //icon: 'images/bart-icon.png',
-                    animation: google.maps.Animation.DROP,
-                    //animation: google.maps.Animation.BOUNCE,
-                    // icon: {
-                    //     url: '/images/bart-icon.png',
-                    //     // This marker is 20 pixels wide by 32 pixels high.
-                    //     size: new google.maps.Size(20, 32),
-                    //     // The origin for this image is (0, 0).
-                    //     origin: new google.maps.Point(0, 0),
-                    //     // The anchor for this image is the base of the flagpole at (0, 32).
-                    //     anchor: new google.maps.Point(0, 32),
-                    //     labelOrigin: new google.maps.Point(10, 20),
+                    var zIndex = index * position.id.length;
 
-                    //   }
-                    zIndex: zIndex
-                });
+                    var marker = new google.maps.Marker({
+                        map: map,
+                        title: 'Nome do herói: '+position.id,
+                        position: {
+                            lat: position.lat,
+                            lng: position.lng
+                        },
+                        label: {
+                            //color: '',
+                            //fontFamily: '',
+                            fontSize: '18px',
+                            //fontWeight: '2px',
+                            text: position.id.substring(0, 3)
+                        },
+                        //label: '<div style="font-size:20px;">'+position.id+'</div>',
+                        //icon: 'images/bart-icon.png',
+                        animation: google.maps.Animation.DROP,
+                        //animation: google.maps.Animation.BOUNCE,
+                        // icon: {
+                        //     url: '/images/bart-icon.png',
+                        //     // This marker is 20 pixels wide by 32 pixels high.
+                        //     size: new google.maps.Size(20, 32),
+                        //     // The origin for this image is (0, 0).
+                        //     origin: new google.maps.Point(0, 0),
+                        //     // The anchor for this image is the base of the flagpole at (0, 32).
+                        //     anchor: new google.maps.Point(0, 32),
+                        //     labelOrigin: new google.maps.Point(10, 20),
 
-                localPositions.push({
-                    id: position.id,
-                    marker: marker
-                });
+                        //   }
+                        zIndex: zIndex
+                    });
+
+                    localPositions.push({
+                        id: position.id,
+                        marker: marker
+                    });
+                }
             }
         }
     }
 
+    // retira os marcadores que não estão no servidor
     for(var localIndex in localPositions)
     {
-        var index = positions.findIndex(position => position.id === localPosition.id);
-            
-        if(index < 0)
+        if(localPositions[localIndex])
         {
-            localPositions[localIndex].marker.setMap(null);
-            localPositions[localIndex] = null;
+            var index = positions.findIndex(position => position.id === localPositions[localIndex].id);
+                
+            if(index < 0)
+            {   
+            
+                if(localPositions[localIndex].id === myId)
+                {
+                    // para o watch antes de catar nova location
+                    navigator.geolocation.clearWatch(positionWatch);
+                    navigator.geolocation.getCurrentPosition((position) => {
+                        uploadMyPosition(position);
+                        positionWatch = navigator.geolocation.watchPosition(uploadMyPosition);
+                    });
+                }
+                else
+                {
+                    localPositions[localIndex].marker.setMap(null);
+                    localPositions[localIndex] = null;
+                    localPositions.splice(localIndex,1);
+                }
+            }
         }
-   }
+    }
 }
 
-function updateUserName()
+function login()
 {
     var userName = document.getElementById('userName').value;
 
-    if(userName != "" && userName != myLastId)
+    if(userName != "")
     {
         myId = userName;
+        window.localStorage.setItem('myId', userName);
 
-        clearInterval(uploadMyPositionTimer);
-        uploadMyPositionTimer = setInterval(uploadMyPosition, uploadInterval);
-        clearInterval(updatePositionsTimer);
-        updatePositionsTimer = setInterval(updatePositions, updateInterval);
+        finishLogin();
+        initMap();
     }    
+}
+
+function initMap()
+{
+    navigator.geolocation.getCurrentPosition((position) => {
+
+        var divMap = document.createElement("div");
+        divMap.id = "map";
+        document.body.appendChild(divMap);
+
+        map = new google.maps.Map(divMap, {
+            center: {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            },
+            zoom: 15
+        });
+
+        uploadMyPosition(position);
+
+        positionWatch = navigator.geolocation.watchPosition(uploadMyPosition);
+    });
+    
+    updatePositionsTimer = setInterval(updatePositions, updateInterval);
+}
+
+function initLogin()
+{
+    var idBar = document.createElement("div");
+    idBar.innerHTML = '\
+        <div id="loginBox">\
+            <span id="loginTitle"> Insira aqui o no de usuario que voce quer utilizar</span></br>\
+            <div id="loginBar">\
+                <input id="userName" class="form-control" type="text" placeholder="Insert your user name (ex: joao)">\
+                <button id="goButton" class="btn btn-default" type="button" onclick="login()">Go!</button>\
+            </div>\
+        </div>';
+    idBar.id = "loginPage";
+    document.body.appendChild(idBar);
+}
+
+function finishLogin()
+{
+    var idBar = document.getElementById("loginPage");
+    document.body.removeChild(idBar);
 }
--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,11 +1,10 @@
 var server = true;
 var url = "https://findserver.azurewebsites.net/positions";
-var localPositions = [];
-var uploadMyPositionTimer;
-var updatePositionsTimer;
-var updateInterval = 1000;
-var uploadInterval = 1000;
 var minimumPrecision = 0.0001;
+var updateInterval = 1000;
+var updatePositionsTimer;
+var localPositions = [];
+var positionWatch;
 var myPosition;
 var myId;
 var map;
@@ -19,28 +18,6 @@ function onMapReady() {
     else 
         initLogin();    
 }
-
-// function uploadMyPosition()
-// {
-//     navigator.geolocation.getCurrentPosition((position) => {
-
-//         if(!myPosition || Math.abs(position.coords.latitude - myPosition.lat) > minimumPrecision || Math.abs(position.coords.longitude - myPosition.lng) > minimumPrecision)
-//         {
-//             myPosition = {lat: position.coords.latitude, lng: position.coords.longitude};
-            
-//             var data = {id: window.localStorage.getItem('myId'), lat: myPosition.lat, lng: myPosition.lng};
-
-//             $.ajax({
-//                 url: url,
-//                 type: "POST",
-//                 data: JSON.stringify(data),
-//                 dataType: "json",
-//                 contentType: "application/json; charset=utf-8",
-//                 success: function(data, status){}
-//             });
-//         }
-//     });
-// }
 
 function uploadMyPosition(position)
 {
@@ -71,79 +48,101 @@ function updatePositions()
 
 function updateMarkers(positions)
 {
-    for(var position of positions)
+    if(positions.length > 0)
     {
-        if(position.id)    
+        for(var position of positions)
         {
-            var index = localPositions.findIndex(localPosition => localPosition.id === position.id);
-            
-            if(index > -1)
+            if(position.id)    
             {
-                var lat = localPositions[index].marker.getPosition().lat();
-                var lng = localPositions[index].marker.getPosition().lng();
-            
-                if(Math.abs(lat - position.lat) > minimumPrecision || Math.abs(lng - position.lng) > minimumPrecision)
-                    localPositions[index].marker.setPosition({lat: position.lat, lng: position.lng});
-            }
-            else
-            {
-                var alphabet = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+                var index = localPositions.findIndex(localPosition => localPosition.id === position.id);
+                
+                if(index > -1)
+                {
+                    var lat = localPositions[index].marker.getPosition().lat();
+                    var lng = localPositions[index].marker.getPosition().lng();
+                    
 
-                var index = alphabet.findIndex(letter => letter === position.id[0].toLowerCase());
+                    if(Math.abs(lat - position.lat) > minimumPrecision || Math.abs(lng - position.lng) > minimumPrecision)
+                        localPositions[index].marker.setPosition({lat: position.lat, lng: position.lng});
+                }
+                else
+                {
+                    var alphabet = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-                if(index < 1)
-                    index = 1;
+                    var index = alphabet.findIndex(letter => letter === position.id[0].toLowerCase());
 
-                var zIndex = index * position.id.length;
+                    if(index < 1)
+                        index = 1;
 
-                var marker = new google.maps.Marker({
-                    map: map,
-                    title: 'Nome do herói: '+position.id,
-                    position: {
-                        lat: position.lat,
-                        lng: position.lng
-                    },
-                    label: {
-                        //color: '',
-                        //fontFamily: '',
-                        fontSize: '18px',
-                        //fontWeight: '2px',
-                        text: position.id.substring(0, 3)
-                    },
-                    //label: '<div style="font-size:20px;">'+position.id+'</div>',
-                    //icon: 'images/bart-icon.png',
-                    animation: google.maps.Animation.DROP,
-                    //animation: google.maps.Animation.BOUNCE,
-                    // icon: {
-                    //     url: '/images/bart-icon.png',
-                    //     // This marker is 20 pixels wide by 32 pixels high.
-                    //     size: new google.maps.Size(20, 32),
-                    //     // The origin for this image is (0, 0).
-                    //     origin: new google.maps.Point(0, 0),
-                    //     // The anchor for this image is the base of the flagpole at (0, 32).
-                    //     anchor: new google.maps.Point(0, 32),
-                    //     labelOrigin: new google.maps.Point(10, 20),
+                    var zIndex = index * position.id.length;
 
-                    //   }
-                    zIndex: zIndex
-                });
+                    var marker = new google.maps.Marker({
+                        map: map,
+                        title: 'Nome do herói: '+position.id,
+                        position: {
+                            lat: position.lat,
+                            lng: position.lng
+                        },
+                        label: {
+                            //color: '',
+                            //fontFamily: '',
+                            fontSize: '18px',
+                            //fontWeight: '2px',
+                            text: position.id.substring(0, 3)
+                        },
+                        //label: '<div style="font-size:20px;">'+position.id+'</div>',
+                        //icon: 'images/bart-icon.png',
+                        animation: google.maps.Animation.DROP,
+                        //animation: google.maps.Animation.BOUNCE,
+                        // icon: {
+                        //     url: '/images/bart-icon.png',
+                        //     // This marker is 20 pixels wide by 32 pixels high.
+                        //     size: new google.maps.Size(20, 32),
+                        //     // The origin for this image is (0, 0).
+                        //     origin: new google.maps.Point(0, 0),
+                        //     // The anchor for this image is the base of the flagpole at (0, 32).
+                        //     anchor: new google.maps.Point(0, 32),
+                        //     labelOrigin: new google.maps.Point(10, 20),
 
-                localPositions.push({
-                    id: position.id,
-                    marker: marker
-                });
+                        //   }
+                        zIndex: zIndex
+                    });
+
+                    localPositions.push({
+                        id: position.id,
+                        marker: marker
+                    });
+                }
             }
         }
     }
 
+    // retira os marcadores que não estão no servidor
     for(var localIndex in localPositions)
     {
-        var index = positions.findIndex(position => position.id === localPositions[localIndex].id);
-            
-        if(index < 0 && localPositions[localIndex].id != myId)
+        if(localPositions[localIndex])
         {
-            localPositions[localIndex].marker.setMap(null);
-            localPositions[localIndex] = null;
+            var index = positions.findIndex(position => position.id === localPositions[localIndex].id);
+                
+            if(index < 0)
+            {   
+            
+                if(localPositions[localIndex].id === myId)
+                {
+                    // para o watch antes de catar nova location
+                    navigator.geolocation.clearWatch(positionWatch);
+                    navigator.geolocation.getCurrentPosition((position) => {
+                        uploadMyPosition(position);
+                        positionWatch = navigator.geolocation.watchPosition(uploadMyPosition);
+                    });
+                }
+                else
+                {
+                    localPositions[localIndex].marker.setMap(null);
+                    localPositions[localIndex] = null;
+                    localPositions.splice(localIndex,1);
+                }
+            }
         }
     }
 }
@@ -179,11 +178,10 @@ function initMap()
         });
 
         uploadMyPosition(position);
+
+        positionWatch = navigator.geolocation.watchPosition(uploadMyPosition);
     });
-
-    //uploadMyPositionTimer = setInterval(uploadMyPosition, uploadInterval);
-    navigator.geolocation.watchPosition(uploadMyPosition);
-
+    
     updatePositionsTimer = setInterval(updatePositions, updateInterval);
 }
 
@@ -192,7 +190,7 @@ function initLogin()
     var idBar = document.createElement("div");
     idBar.innerHTML = '\
         <div id="loginBox">\
-            <span id="loginTitle"> Insira aqui o seu nome de usuário</span></br>\
+            <span id="loginTitle"> Insira aqui o no de usuario que voce quer utilizar</span></br>\
             <div id="loginBar">\
                 <input id="userName" class="form-control" type="text" placeholder="Insert your user name (ex: joao)">\
                 <button id="goButton" class="btn btn-default" type="button" onclick="login()">Go!</button>\
